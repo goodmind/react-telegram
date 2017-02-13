484f937da3838e9e8a4b637266d2d7aac23f3261
goodmind <andwebar@gmail.com>
2017-02-14 02:57:36 +0500

    refactor(api): render nodes into telegram

--- src/api/index.js
+++ src/api/index.js
@@ -1,16 +1,60 @@
+import { Message } from './widgets/Message'
+import { InlineKeyboardMarkup } from './widgets/InlineKeyboardMarkup'
+import { InlineButton } from './widgets/InlineButton'
+
 export class Screen {
-  constructor () {
+  constructor (bot) {
     this.__telegram_screen = true
+    this.bot = bot
     this.children = []
   }
 
+  renderMessage (msg) {
+    const markup = msg.children[0]
+    return this.bot.sendMessage(msg.to.id, msg.content, { markup: this.renderNode(markup) }).then(({ result: m }) => {
+      this.bot.callbackStorage[m.message_id] = (cb) => {
+        const btn = markup.children[0]
+        btn.emit('event', 'click', {
+          button: 0,
+          defaultPrevented: false,
+          preventDefault: () => {}
+        })
+      }
+    })
+  }
+
+  renderNode (node) {
+    if (node instanceof Message) {
+      return this.renderMessage(node)
+    }
+    if (node instanceof InlineKeyboardMarkup) {
+      return this.bot.inlineKeyboard([
+        node.children.map(this.renderNode, this)
+      ])
+    }
+    if (node instanceof InlineButton) {
+      return this.bot.inlineButton('Command button', { callback: '/hello' })
+    }
+    return undefined
+  }
+
   append (node) {
+    node.parent = this
     this.children.push(node)
   }
 
+  remove (node) {
+    if (node.parent !== this) return
+    const i = this.children.indexOf(node)
+    if (!~i) return
+
+    node.parent = null
+    this.children.splice(i, 1)
+  }
+
   render (...args) {
-    console.log('Rendering...')
-    console.log(this.children[0])
+    const node = this.children[0]
+    return this.renderNode(node)
   }
 }
 
