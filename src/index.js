f357ed6c8877ae20440bcf9d1e6ce9e9f8eacde0
goodmind <andwebar@gmail.com>
2017-02-14 02:56:39 +0500

    refactor(index): handle commands

--- src/index.js
+++ src/index.js
@@ -1,2 +1,44 @@
-export { render } from './renderer/render'
+import TeleBot from 'telebot'
+import invariant from 'invariant'
+import { render } from './renderer/render'
+
+const splitIn = str => str.indexOf(' ') !== -1 ? [
+  str.substr(0, str.indexOf(' ')),
+  str.substr(str.indexOf(' ') + 1)
+] : [str]
+
+export class Telegram extends TeleBot {
+  constructor (options, history) {
+    super(options)
+    invariant(
+      history !== undefined,
+      'Telegram: You must pass a valid History.'
+    )
+
+    this.callbackStorage = {}
+    this.history = history
+    this.render = render
+    this.registerCallbacks()
+  }
+
+  registerCallbacks () {
+    this.on('callbackQuery', cb => {
+      const callback = this.callbackStorage[cb.message.message_id]
+      if (callback) callback(cb)
+    })
+
+    this.on('/*', msg => {
+      const [url, data] = splitIn(msg.text)
+      if (!this.history.getCurrentLocation) {
+        this.history.push(url, { data, msg })
+      } else {
+        this.history.push({
+          pathname: url,
+          state: { data, msg }
+        })
+      }
+    })
+  }
+}
+
 export { Screen } from './api'
