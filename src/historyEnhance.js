5f19df7bc7531cc191301c1905eea052d1a3fe1b
goodmind <andwebar@gmail.com>
2017-02-16 21:50:03 +0500

    fix(historyEnhance): store only msg

--- src/historyEnhance.js
+++ src/historyEnhance.js
@@ -1,10 +1,12 @@
-let prevState
+let prevState = {
+  msg: null
+}
 
 const enhanceV4 = history => ({
   ...history,
   push (path, state = prevState) {
     const r = history.push(path, state)
-    prevState = state
+    prevState.msg = state.msg
     return r
   }
 })
@@ -14,7 +16,7 @@ const enhanceV3 = history => ({
   push (input) {
     const newInput = typeof input === 'string' ? { pathname: input, state: prevState } : input
     const r = history.push(newInput)
-    prevState = newInput.state || prevState
+    prevState.msg = (newInput.state || prevState).msg
     return r
   }
 })
