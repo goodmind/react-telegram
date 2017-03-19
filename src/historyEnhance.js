814273ef8786a6174d78059ceda13645b88b3431
goodmind <andwebar@gmail.com>
2017-03-19 10:01:31 +0500

    refactor(historyEnhance): wrap in function

--- src/historyEnhance.js
+++ src/historyEnhance.js
@@ -1,27 +1,27 @@
-let prevState = {
-  msg: null
-}
-
-const enhanceV4 = history => ({
-  ...history,
-  push (path, state = prevState) {
-    const r = history.push(path, state)
-    prevState.msg = state.msg
-    return r
-  }
-})
-
-const enhanceV3 = history => ({
-  ...history,
-  push (input) {
-    const newInput = typeof input === 'string' ? { pathname: input, state: prevState } : input
-    const r = history.push(newInput)
-    prevState.msg = (newInput.state || prevState).msg
-    return r
-  }
-})
-
 export default function historyEnhance (history, version) {
+  let prevState = {
+    msg: null
+  }
+
+  const enhanceV4 = history => ({
+    ...history,
+    push (path, state = prevState) {
+      const r = history.push(path, state)
+      prevState.msg = state.msg
+      return r
+    }
+  })
+
+  const enhanceV3 = history => ({
+    ...history,
+    push (input) {
+      const newInput = typeof input === 'string' ? { pathname: input, state: prevState } : input
+      const r = history.push(newInput)
+      prevState.msg = (newInput.state || prevState).msg
+      return r
+    }
+  })
+
   const enhance = version === 3 ? enhanceV3 : enhanceV4
   return enhance(history)
 }
